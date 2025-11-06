d4036097385ec8f4e7faf129b4dbbf6613f86798
Raphaël Desprez-Le Goarant <rdesprez@student.42lyon.fr>
2025-11-06 13:35:44 +0100

    Use router, implement home view

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const num = ref(0)
+
+function randomNumber() {
+  num.value = Math.random()
+}
+</script>
+
+<template>
+  <h1>Hello, world!</h1>
+  <p>Number is: {{ num }}</p>
+  <button @click="randomNumber">Random</button>
+</template>
+
+<style scoped></style>
